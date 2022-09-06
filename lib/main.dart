7ba21b01e0896466ca0f575e8431f197700b0085
Alishakhan21 <alisha.esposfttech@gmail.com>
2022-09-06 14:47:20 +0530

    TESTING

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Github Actions'),
+      home: const MyHomePage(title: 'Github Actions'),
     );
   }
 }
